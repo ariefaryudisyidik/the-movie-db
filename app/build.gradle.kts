281edabda209dbb5db23012a1de2c23abfbf432a
Arief Aryudi Syidik <ariefaryudisyidik@gmail.com>
2022-11-02 15:56:36 +0700

    feat: Add lottie library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation(libs.androidx.room.paging)
     implementation(libs.glide)
     implementation(libs.hilt.android)
+    implementation(libs.lottie)
     implementation(libs.material)
     implementation(libs.okhttp3.logging.interceptor)
     implementation(libs.retrofit2.converter.gson)
