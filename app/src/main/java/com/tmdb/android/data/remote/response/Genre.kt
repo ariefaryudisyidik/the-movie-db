6022c3083795a646f367657757ad9ba5c3c5f4df
Arief Aryudi Syidik <ariefaryudisyidik@gmail.com>
2022-11-03 15:56:37 +0700

    fix: Add video response

---
+++ app/src/main/java/com/tmdb/android/data/remote/response/Genre.kt
@@ -0,0 +1,6 @@
+package com.tmdb.android.data.remote.response
+
+data class Genre(
+    val id: Int,
+    val name: String
+)
--- app/src/main/java/com/tmdb/android/data/remote/response/MovieResponse.kt
+++ app/src/main/java/com/tmdb/android/data/remote/response/MovieResponse.kt
@@ -1,6 +1,5 @@
 package com.tmdb.android.data.remote.response
 
-
 import com.google.gson.annotations.SerializedName
 
 data class MovieResponse(
