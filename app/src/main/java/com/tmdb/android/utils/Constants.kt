274615154888b8b31dbaf5e137c395e3cba1c731
Arief Aryudi Syidik <ariefaryudisyidik@gmail.com>
2022-11-07 11:23:10 +0700

    feat: Change image url resolution

--- app/src/main/java/com/tmdb/android/utils/Constants.kt
+++ app/src/main/java/com/tmdb/android/utils/Constants.kt
@@ -2,6 +2,6 @@ package com.tmdb.android.utils
 
 const val DATABASE_NAME = "movie.db"
 const val QUERY_API_KEY = "api_key"
-const val IMAGE_URL = "https://image.tmdb.org/t/p/w500/"
+const val IMAGE_URL = "https://image.tmdb.org/t/p/w1280/"
 const val INITIAL_PAGE_INDEX = 1
 const val PAGE_SIZE = 10
